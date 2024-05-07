e65b8a0288ba151083086fd56f5a6357cd6fc330
Amelia Mesich <amelia.mesich@mail.utoronto.ca>
2024-05-07 12:02:46 -0400

    Fixing the bash script May 7th

--- scripts/Amelia_bash.sh
+++ scripts/Amelia_bash.sh
@@ -1,21 +1,21 @@
-!/bin/bash
+#!/bin/bash
 
-SBATCH --job-name=Artiodactyla_diel_models           #This is the name of your job
-SBATCH --cpus-per-task=1                  #This is the number of cores reserved
-SBATCH --mem-per-cpu=16G              #This is the memory reserved per core.
-SBATCH --tmp=16G
-Total memory reserved: 16GB
+#SBATCH --job-name=Artiodactyla_diel_models           #This is the name of your job
+#SBATCH --cpus-per-task=1                  #This is the number of cores reserved
+#SBATCH --mem-per-cpu=16G              #This is the memory reserved per core.
+#SBATCH --tmp=16G
+#Total memory reserved: 16GB
 
-SBATCH --time=24:00:00        #This is the time that your task will run
-SBATCH --qos=1day           #You will run in this queue
+#SBATCH --time=24:00:00        #This is the time that your task will run
+#SBATCH --qos=1day           #You will run in this queue
 
 # Paths to STDOUT or STDERR files should be absolute or relative to current working directory
 #SBATCH --output=/scicore/home/schiera/gizevo30/projects/fish_sleep/scripts/logs/Amelia_artiodactyla_models_stdout.txt
 #SBATCH --error=/scicore/home/schiera/gizevo30/projects/fish_sleep/scripts/logs/Amelia_artiodactyla_models_stderr.txt
 
 #You selected an array of jobs from 1 to n with n simultaneous jobs
-SBATCH --mail-type=END,FAIL,TIME_LIMIT,COMPLETE
-SBATCH --mail-user=amelia.mesich@mail.utoronto.ca       #You will be notified via email when your task ends or fails
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT,COMPLETE
+#SBATCH --mail-user=amelia.mesich@mail.utoronto.ca       #You will be notified via email when your task ends or fails
 
 #This job runs from the current working directory
 
