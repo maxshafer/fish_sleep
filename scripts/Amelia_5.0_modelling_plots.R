f619b61b793cc43986fd130a1a8d520eae1a5deb
Amelia Mesich <amelia.mesich@mail.utoronto.ca>
2025-08-19 16:32:12 -0400

    fixing the transition rate plots. August 19th

--- scripts/Amelia_5.0_modelling_plots.R
+++ scripts/Amelia_5.0_modelling_plots.R
@@ -44,14 +44,22 @@ dev.off()
 
 
 # Section 3: Load in and format data --------------------------------------
-model_1_500 <- readRDS(here("august_12__whippomorpha_four_state_max_crep_traits_ER_SYM_ARD_CONSYM_bridge_only_models.rds"))
-model_501_1000 <- readRDS(here("august_14__whippomorpha_four_state_max_crep_traits_ER_SYM_ARD_CONSYM_bridge_only_models.rds"))
-model_results <- c(model_1_500, model_501_1000)
-saveRDS(model_results, here("august_whippomorpha_four_state_max_crep_traits_ER_SYM_ARD_CONSYM_bridge_only_models.rds"))
+model_results1 <- readRDS(here("august_12__whippomorpha_four_state_max_crep_traits_ER_SYM_ARD_CONSYM_bridge_only_models.rds"))
+model_results2 <- readRDS(here("august_14__whippomorpha_four_state_max_crep_traits_ER_SYM_ARD_CONSYM_bridge_only_models.rds"))
+model_results1$ER_model <- c(model_results1$ER_model, model_results2$ER_model)
+model_results1$SYM_model <- c(model_results1$SYM_model, model_results2$SYM_model)
+model_results1$ARD_model <- c(model_results1$ARD_model, model_results2$ARD_model)
+model_results1$bridge_only_model <- c(model_results1$bridge_only_model, model_results2$bridge_only_model)
+model_results1$CONSYM_model <- c(model_results1$CONSYM_model, model_results2$CONSYM_model)
+
+saveRDS(model_results1, here("august_whippomorpha_four_state_max_crep_traits_ER_SYM_ARD_CONSYM_bridge_only_models.rds"))
+
 filename <- "august_whippomorpha_four_state_max_crep_traits_ER_SYM_ARD_CONSYM_bridge_only_models.rds"
+model_results <- readRDS(here("august_12__whippomorpha_four_state_max_crep_traits_ER_SYM_ARD_CONSYM_bridge_only_models.rds"))
+model_results <- readRDS(here("august_whippomorpha_four_state_max_crep_traits_ER_SYM_ARD_CONSYM_bridge_only_models.rds"))
 
 # Section 4: Plot AIC scores from 1k model results ----------------------
-#requires the filename and the number of Mk models (3: ER, SYM, ARD or 4: ER, SYM, ARD, bridge_only)
+#requires the filename and the number of Mk models (3: ER, SYM, ARD or 4: ER, SYM, ARD, CONARD, 5: ER, SYM, ARD, bridge_only, CONSYM)
 #returns a df of the AIC scores for all 1k trees x number of Mk models
 df_full <- plot1kAIC(readRDS(here(filename)), 5)
 
@@ -59,7 +67,7 @@ means <- aggregate(AIC_score ~  model, df_full, mean)
 means$AIC_score <- round(means$AIC_score, digits = 2)
 
 #plot and save out - boxplot
-png(paste0("C:/Users/ameli/OneDrive/Documents/R_projects/finalized_results_plots/", filename, "_AIC", ".png"), width = 20, height = 20, units = "cm", res = 400)
+png(paste0("C:/Users/ameli/OneDrive/Documents/R_projects/Amelia_figures/", filename, "_AIC", ".png"), width = 20, height = 20, units = "cm", res = 400)
 ggplot(df_full, aes(x = fct_inorder(model), y = AIC_score)) + geom_jitter(alpha = 0.6, color = "royalblue1") + #other colour option 766df8 and 6daaf8
   geom_boxplot(alpha = 0.5, outlier.shape = NA, colour = "black") +
   theme(axis.text.x = element_text(angle = 0, vjust = 0, hjust=0, size = 10), axis.title = element_text(size = 12)) +
@@ -69,16 +77,8 @@ ggplot(df_full, aes(x = fct_inorder(model), y = AIC_score)) + geom_jitter(alpha 
   ggtitle(filename) 
 dev.off()
 
-means <- aggregate(AIC_score ~  model, df_full, mean)
-means$AIC_score <- as.integer(means$AIC_score)
-
 #plot and save out - raincloud plot, ruminant format
-filename1 <- "fixed_whippomorpha_four_state_max_crep_traits_ER_SYM_ARD_bridge_only_CONSYM_models.rds"
-df_full <- plot1kAIC(readRDS(here(filename)), 5)
-means <- aggregate(AIC_score ~  model, df_full1, mean)
-means$AIC_score <- round(means$AIC_score, digits = 2)
-
-png(paste0("C:/Users/ameli/OneDrive/Documents/R_projects/finalized_results_plots/", "1_test_", filename1, "_AIC", ".png"), width = 20, height = 15, units = "cm", res = 600)
+png(paste0("C:/Users/ameli/OneDrive/Documents/R_projects/Amelia_figures/", "raincloud", filename, "_AIC", ".png"), width = 20, height = 15, units = "cm", res = 600)
 ggplot(df_full, aes(x = fct_inorder(model), y = AIC_score, fill = fct_inorder(model))) + 
   scale_fill_manual(values = c("blue", "royalblue","slateblue", "mediumpurple", "orchid")) +
   ggdist::stat_halfeye(alpha = 0.6, adjust = .5, width = .6, justification = -.3, .width = 0, point_colour = NA) +
@@ -93,37 +93,16 @@ ggplot(df_full, aes(x = fct_inorder(model), y = AIC_score, fill = fct_inorder(mo
 #coord_cartesian(xlim = c(1.4, 4.3), ylim = c(342, 451))
 dev.off()
 
-#plot and save out - raincloud plot, whippo format
-filename2 <- "fixed_whippomorpha_four_state_max_crep_traits_ER_SYM_ARD_bridge_only_CONSYM_models.rds"
-df_full2 <- plot1kAIC(readRDS(here(paste0("finalized_1k_models/", filename2))), 4)
-means <- aggregate(AIC_score ~  model, df_full2, mean)
-means$AIC_score <- round(means$AIC_score, digits = 2)
+#why is the stat summary mean different than the boxplot mean
+ggplot(df_full, aes(y = AIC_score, x = model))+ geom_boxplot() + stat_summary(fun = "mean", geom="point", size=2, colour = "red")
 
-png(paste0("C:/Users/ameli/OneDrive/Documents/R_projects/finalized_results_plots/", "1_test_", filename2, "_AIC", ".png"), width = 20, height = 15, units = "cm", res = 600)
-ggplot(df_full2, aes(x = fct_inorder(model), y = AIC_score, fill = fct_inorder(model))) + 
-  scale_fill_manual(values = c("blue", "royalblue","slateblue", "mediumpurple")) +
-  ggdist::stat_halfeye(alpha = 0.6, adjust = .5, width = 0.6, justification = -.3, .width = 0, point_colour = NA) +
-  geom_boxplot(alpha = 0.2, width = .25, colour = "black",outlier.shape = NA) + 
-  geom_point(aes(color = fct_inorder(model)), stroke = 1, size = 1, alpha = .2, position = position_jitter(seed = 1, width = .15)) +
-  scale_color_manual(values = c("blue", "royalblue","slateblue", "mediumpurple")) +
-  labs(x = "Model", y = "AIC scores")  +
-  scale_x_discrete(labels = c("ER", "SYM", "ARD", "CON-ARD")) +
-  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 18), legend.position = "none") +
-  geom_text(data = means, aes(label = AIC_score, y = AIC_score, hjust = -1)) +
-  ggtitle(filename2) + 
-  coord_cartesian(xlim = c(1.4, 4.0), ylim = c(202, 233))
-dev.off()
-
-ggplot(df_full2, aes(y = AIC_score, x = model))+ geom_boxplot() + stat_summary(fun = "mean", geom="point", size=2, colour = "red")
+#what is the most likely model on the most likely tree?
+min(df_full$AIC_score)
 
 # Section 5: Plot AICc scores from 1k model results ----------------------
-#filename <- "finalized_1k_models/artiodactyla_three_state_max_crep_traits_ER_SYM_ARD_bridge_only_models.rds"
-
 #requires the filename and the number of Mk models (3: ER, SYM, ARD or 4: ER, SYM, ARD, bridge_only)
 #returns a df of the AICc scores for all 1k trees x number of Mk models
-df_full <- plot1kAICc(readRDS(here(paste0("finalized_1k_models/", filename))), 5)
-df_full <- plot1kAICc(model_results, 5)
-
+df_full <- plot1kAICc(readRDS(here(filename)), 5)
 means <- aggregate(AICc_score ~  model, df_full, mean)
 means$AICc_score <- round(means$AICc_score, digits = 2)
 
@@ -136,23 +115,21 @@ ggplot(df_full, aes(x = fct_inorder(model), y = AICc_score)) + geom_jitter(alpha
 dev.off()
 
 # Section 6: Plot transition rates from 1k model results ----------------------
-#filename <- "artiodactyla_three_state_max_crep_traits_ER_SYM_ARD_bridge_only_models.rds"
-
 #requires the filename, the number of states in the model and the number of Mk models 
 #returns a dataframe of the rates from each of the Mk models, for each of the 1k trees
 #filename <- "ruminants_four_state_max_crep_ER_SYM_ARD_bridge_only_models.rds"
 #reminder: enter 5 states for the 6 state cetacean/whippomorpha
-rates_df <- plot1kTransitionRates(model_results, 4, 5)
+rates_df <- plot1kTransitionRates(readRDS(here(filename)), 4, 5)
 
 #plot as a violin plot
 #important note: the colours column doesn't line up with the correct solution in the df but if we plot the solutions in alphabetical order and then colouring them with the pallette in the colours column is in the correct order
-png(paste0("C:/Users/ameli/OneDrive/Documents/R_projects/finalized_results_plots/", filename, "_violin_rate_plot_all", ".png"), width = 40, height = 20, units = "cm", res = 600)
-ggplot(rates_df, aes(x= solution, y = log(rates), group = solution, fill = solution, colour = solution)) + geom_jitter(aes(alpha = 0.1)) + scale_color_manual(values = rates_df$colours) + geom_violin(color = "black", scale = "width") + theme(axis.text.x = element_text(angle = 90, vjust = 0, hjust=1, size =10), axis.text.y = element_text(size =10))  + scale_fill_manual(values = rates_df$colours) + theme(legend.position = "none") + labs(x = "Transition", y = "Log(rates)") + stat_summary(fun=median, geom="point", size=2, colour = "red") + ggtitle(filename) + facet_wrap(~fct_inorder(model), ncol = 2, nrow = 2) 
+png(paste0("C:/Users/ameli/OneDrive/Documents/R_projects/Amelia_figure/", filename, "_violin_rate_plot_all", ".png"), width = 40, height = 20, units = "cm", res = 600)
+ggplot(rates_df, aes(x= solution, y = log(rates), group = solution, fill = solution, colour = solution)) + geom_jitter(aes(alpha = 0.1)) + scale_color_manual(values = rates_df$colours) + geom_violin(color = "black", scale = "width") + theme(axis.text.x = element_text(angle = 90, vjust = 0, hjust=1, size =10), axis.text.y = element_text(size =10))  + scale_fill_manual(values = rates_df$colours) + theme(legend.position = "none") + labs(x = "Transition", y = "Log(rates)") + stat_summary(fun=median, geom="point", size=2, colour = "red") + ggtitle(filename) + facet_wrap(~fct_inorder(model), ncol = 3, nrow = 2) 
 dev.off()
 
 #violin plots for each Mk model
 for(i in 1:length(unique(rates_df$model))){
-  png(paste0("C:/Users/ameli/OneDrive/Documents/R_projects/finalized_results_plots/", filename, "_violin_rate_plot_", unique(rates_df$model)[i], ".png"), width = 25, height = 12, units = "cm", res = 600)
+  png(paste0("C:/Users/ameli/OneDrive/Documents/R_projects/Amelia_figure/", filename, "_violin_rate_plot_", unique(rates_df$model)[i], ".png"), width = 25, height = 12, units = "cm", res = 600)
   print(ggplot(rates_df, aes(x= solution, y = log(rates), group = solution, fill = solution, colour = solution)) + geom_jitter(aes(alpha = 0.1)) + scale_color_manual(values = rates_df$colours) + geom_violin(color = "black", scale = "width") + theme(axis.text.x = element_text(angle = 90, vjust = 0, hjust=1, size =10), axis.text.y = element_text(size =10))  + scale_fill_manual(values = rates_df$colours) + theme(legend.position = "none") + labs(x = "Transition", y = "Log(rates)") + stat_summary(fun=median, geom="point", size=2, colour = "red") + ggtitle(filename) + facet_wrap_paginate(~fct_inorder(model), ncol = 1, nrow = 1, page = i)) 
   dev.off()
 }
@@ -170,8 +147,6 @@ for(i in 1:(length(unique(rates_df$model)))){
   print(rates_df %>% filter(model == unique(rates_df$model)[i]) %>% ggplot(., aes(x= rates, fill = solution)) + geom_histogram() + scale_fill_manual(values = rates_df$colours) + scale_x_continuous(trans='log10', labels = trans_format("log10", math_format(10^.x))) + theme_bw() + theme(plot.title = element_blank(), legend.position = "none") + labs(title = filename, y = "count" , x = "transition rate per unit of evolutionary time") + facet_wrap(~solution))
   dev.off()
 }
-
-
 
 
 
